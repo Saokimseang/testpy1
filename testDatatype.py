9b5c45d8f29739df6379801fdb1ff8e37f450880
Saokimseang <kokinglove5@gmail.com>
2024-07-20 11:29:49 +0700

    Value

--- testDatatype.py
+++ testDatatype.py
@@ -1,10 +1,11 @@
-str="hello world"
-print(str)
-print(str[0])
-print(str[2:5])
-print(str[2:])
-print(str*10)
-print(str+" Test  ")
+#string
+str_text="hello world"
+print(str_text)
+print(str_text[0])
+print(str_text[2:5])
+print(str_text[2:])
+print(str_text*10)
+print(str_text+" Test  ")
 
 #number
 a=1_000_000
@@ -13,3 +14,10 @@ print(type(a))
 b=13.84
 print(type(b))
 print(b)
+
+val1=int("10")
+val2=int("20")
+total=val1+val2
+print(type(total))
+print("Total =" +str(total))
+print(len(str(total)))
