837db5338dc41d208c3a13609fc1f6da184d2b73
Saokimseang <kokinglove5@gmail.com>
2024-08-17 10:00:28 +0700

    list

---
+++ Random.py
@@ -0,0 +1,7 @@
+import random
+
+ind=random.randint(0,1)
+if ind==0:
+    print("Heads")
+else:
+    print("Leads")
---
+++ list.py
@@ -0,0 +1,11 @@
+list=["abcd", 56,"el","73"]
+
+print(list[0])
+print(list[2])
+print(list[-1])
+
+list.append(100)
+
+list2=[12,32]
+list.extend(list2)
+print(list)
